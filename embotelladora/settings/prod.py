58251edc8baf880819c1b97194e1a5e897642635
Alb4tr02 <jodmunozol@unal.edu.co>
2020-10-10 23:27:50 -0500

    test

--- embotelladora/settings/prod.py
+++ embotelladora/settings/prod.py
@@ -1,13 +1,19 @@
 from .base import *
 import django_on_heroku
-import dj_database_url
-from decouple import config
-
+#import dj_database_url
+#from decouple import config
+import os
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=config('DATABASE_URL')
+#    )
+#}
 SECRET_KEY= 'hlzh$&d2dt%pp2=v+2rl09khxlh(l2)tlknk=ct$oaiu@e)h8_'
 django_on_heroku.settings(locals())
