3256d041281687b7fff7609df1d595dea0225b8e
Alb4tr02 <jodmunozol@unal.edu.co>
2020-12-06 10:32:38 -0500

    heroku

--- embotelladora/settings/prod.py
+++ embotelladora/settings/prod.py
@@ -5,6 +5,13 @@ import dj_database_url
 import os
 
 DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
