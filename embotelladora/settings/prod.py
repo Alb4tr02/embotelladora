0a7f122302794b31807a7c24648dfdd46e2cf308
Alb4tr02 <jodmunozol@unal.edu.co>
2020-12-06 10:29:20 -0500

    heroku

--- embotelladora/settings/prod.py
+++ embotelladora/settings/prod.py
@@ -9,6 +9,5 @@ DEBUG = True
 DATABASES = {
     'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
 }
-#SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = "hlzh$&d2dt%pp2=v+2rl09khxlh(l2)tlknk=ct$oaiu@e)h8_"
+SECRET_KEY = os.getenv('SECRET_KEY')
 django_on_heroku.settings(locals())
