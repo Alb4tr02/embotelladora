b9a398d0d8f15809f33aff216845d7ddf80388df
Alb4tr02 <jodmunozol@unal.edu.co>
2020-12-07 12:47:33 -0500

    comunicacion con flask

--- applications/pedidos/views.py
+++ applications/pedidos/views.py
@@ -4,8 +4,16 @@ from rest_framework.generics import (
     CreateAPIView,
 )
 from .serializers import PedidoCreateSerializer
+import requests
 
 # Create your views here.
 class CreatePedidoApiView(CreateAPIView):
 
     serializer_class = PedidoCreateSerializer
+
+    def post(self, request, *args, **kwargs):
+
+        botellas = request.data.get('cantidad_botellas')
+        response = requests.post('http://192.168.1.11/crear_pedido/' + str(botellas))
+        print(response)
+        return self.create(request, *args, **kwargs)
